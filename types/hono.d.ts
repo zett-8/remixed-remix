3fe365351ed3173003d6176dc2e3b2a2391769c0
zett-8 <kame0008@gmail.com>
2025-03-12 10:39:49 +0100

    delete unneccessary dependencies

--- types/hono.d.ts
+++ types/hono.d.ts
@@ -1,16 +1,7 @@
-import type { D1Database } from '@cloudflare/workers-types/experimental'
-import type { DrizzleD1Database } from 'drizzle-orm/d1'
-import * as schema from '../database/schema/index'
-
 declare global {
   type HonoENV = {
-    Bindings: {
-      DB: D1Database
-    }
-    Variables: {
-      db: DrizzleD1Database<typeof schema>
-      userId: string
-    }
+    Bindings: { _ }
+    Variables: { _ }
   }
 }
 
